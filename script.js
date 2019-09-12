0551aef7ead31b2184968c39b61c8325d010fd0c
Cathrine Thøgersen <cathrinethogersen@Cathrines-MacBook-Air.local>
2019-09-12 13:57:16 +0200

    javascript ændring

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", start);
 const url = `https://spreadsheets.google.com/feeds/list/1OAFNgheFOOnqsYvMPDD6ouyja7FnQMRnThJniKbInvU/od6/public/values?alt=json`;
-let personer;
+let teams;
 let filter = "alle";
 // første funktion der kaldes efter DOM er loaded
 function start() {
     const filterKnapper = document.querySelectorAll("nav button");
-    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerPersoner));
+    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerTeams));
     //document.querySelector("#detalje").style.display = "none";
     skjulDetalje();
     loadData();
 }
 // funktion der filtrerer personer (json)
-function filtrerPersoner() {
-    filter = this.dataset.køn; // sæt variabel "filter" til aktuel værdi
+function filtrerTeams() {
+    filter = this.dataset.kategori; // sæt variabel "filter" til aktuel værdi
     document.querySelector(".valgt").classList.remove("valgt"); // fjern klassen valgt fra aktuel knap
     this.classList.add("valgt") // marker den nyvalgte knap
     vis(); // kald funktionen vis igen med nyt filter
@@ -20,7 +20,7 @@ function filtrerPersoner() {
 // funktion der henter data fra Google sheet (via url)
 async function loadData() {
     const response = await fetch(url);
-    personer = await response.json();
+    teams = await response.json();
     vis();
 }
 //funktion der viser personer i liste view
@@ -29,33 +29,33 @@ function vis() {
     const dest = document.querySelector("#liste"); // container til articles med en person
     const skabelon = document.querySelector("template").content; // select indhold af html skabelon (article)
     dest.textContent = "";
-    personer.feed.entry.forEach(person => { // loop igennem json (personer)
-        if (person.gsx$location.$t == filter || filter == "alle") { // tjek hvilket køn personen har og sammenlign med filter eller vis alle
+    teams.feed.entry.forEach(team => { // loop igennem json (personer)
+        if (team.gsx$location.$t == filter || filter == "alle") { // tjek hvilket køn personen har og sammenlign med filter eller vis alle
             const klon = skabelon.cloneNode(true);
-            klon.querySelector(".navn").textContent = person.gsx$teams.$t;
-            klon.querySelector(".profil-billede").src = "/imgs/" + person.gsx$billede.$t + ".png";
-            klon.querySelector(".person").addEventListener("click", () => {
-                visDetalje(person);
+            klon.querySelector(".navn").textContent = team.gsx$teams.$t;
+            klon.querySelector(".profil-billede").src = "imgs/" + team.gsx$billede.$t + ".png";
+            klon.querySelector(".team").addEventListener("click", () => {
+                visDetalje(team);
             });
             dest.appendChild(klon);
         }
     })
 }
 
-function visDetalje(person) {
-    console.log(person);
+function visDetalje(team) {
+    console.log(team);
 
     document.querySelector("#detalje").style.display = "block";
     document.querySelector("#detalje .luk").addEventListener("click", skjulDetalje);
 
 
-    document.querySelector("#detalje .teams").textContent = person.gsx$teams.$t;
-    document.querySelector("#detalje .win").textContent = "Vundet: " + person.gsx$wins.$t;
-    document.querySelector("#detalje .lose").textContent = "Tabte: " + person.gsx$losses.$t;
-    document.querySelector("#detalje .pts").textContent = "Samlet point: " + person.gsx$pts.$t;
+    document.querySelector("#detalje .teams").textContent = team.gsx$teams.$t;
+    document.querySelector("#detalje .win").textContent = "Vundet: " + team.gsx$wins.$t;
+    document.querySelector("#detalje .lose").textContent = "Tabte: " + team.gsx$losses.$t;
+    document.querySelector("#detalje .pts").textContent = "Samlet point: " + team.gsx$pts.$t;
 
-    document.querySelector("#detalje img").src = "/imgs/" + person.gsx$billede.$t + ".png";
-    document.querySelector("#detalje img").alt = `Portræt af ${person.gsx$billede.$t}`;
+    document.querySelector("#detalje img").src = "large/" + team.gsx$billede.$t + ".png";
+    document.querySelector("#detalje img").alt = `Portræt af ${team.gsx$billede.$t}`;
 
 
 
